4bef4de9155cb6e884005ea27fdda7e44536ef22
Elena Salfiti <elenasalfiti@Elenas-MacBook-Pro.local>
2022-07-20 14:39:06 -0400

    commit

--- algorithm_1.py
+++ algorithm_1.py
@@ -1,27 +1,9 @@
 import re
 
-#task 1
+x = int(input("Write a number of hours:"))
+
 def hours_to_mins(x):
     result = x*60
     return result
 
-mins = hours_to_mins(2)
-print(mins)
-
-#task 2
-n=6
-
-if n>60:
-    hours = n/60
-    print(hours)
-else:
-    mins = n*60 
-    print(mins)
-
-
-#task 3
-def count_letters(word):
-    return len([x for x in word if x.isalpha()])
-
-print(count_letters("Word."))
-
+print(hours_to_mins(x))
